09ea99548ee986008b4e1f9a628803bf0e7b3dfb
Aravindswamymajjuri <aravindswamymajjuri143@gmail.com>
2026-06-30 16:19:02 +0530

    updated the vercel.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  build: {
-    // For Capacitor Android build
-    outDir: mode === "android" ? "../app/www" : "dist",
-    emptyOutDir: true,
-  },
-}));import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  build: {
-    // For Capacitor Android build
-    outDir: mode === "android" ? "../app/www" : "dist",
-    emptyOutDir: true,
-  },
-}));
+});
